389de422b3ae45c33b56bd0c822ad8b5d52ae7c7
Hui Cao <caohui@shgbit.com>
2022-04-18 01:08:43 +0800

    add debugging

--- try/hello/src/main.cpp
+++ try/hello/src/main.cpp
@@ -1,31 +1,59 @@
-#include <iostream>
+#include <sys/stat.h>
+#include <sys/types.h>
+
 #include <ctime>
 #include <fstream>
-#include <string>
+#include <iostream>
 #include <memory>
 #include <opencv2/opencv.hpp>
+#include <string>
 
 using namespace std;
 using namespace cv;
 
+bool init();
+void loop();
 bool save(Mat frame);
 bool display(Mat frame);
 
-template<typename ... Args>
-string string_format(const string& format, Args ... args){
-    size_t size = 1 + snprintf(nullptr, 0, format.c_str(), args ...);  // Extra space for \0
-    // unique_ptr<char[]> buf(new char[size]);
-    char bytes[size];
-    snprintf(bytes, size, format.c_str(), args ...);
-    return string(bytes);
+template <typename... Args>
+string string_format(const string& format, Args... args) {
+  size_t size =
+      1 + snprintf(nullptr, 0, format.c_str(), args...);  // Extra space for \0
+  char bytes[size];
+  snprintf(bytes, size, format.c_str(), args...);
+  return string(bytes);
 }
 
-
 int main() {
+  if (!init()) {
+    return 1;
+  }
+
+  loop();
+
+  return 0;
+}
+
+bool init() {
+  bool result = true;
+  struct stat ret;
+  if (stat("./imgs", &ret) == -1) {
+    if (mkdir("./imgs", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) != 0) {
+      cout << "mkdir imgs failed." << endl;
+      result = false;
+    }
+  } else {
+    cout << "./imgs found." << endl;
+  }
+  return result;
+}
+
+void loop() {
   VideoCapture cap(0);
   if (!cap.isOpened()) {
-    cout << "Cannot open camera\n";
-    return 1;
+    cout << "Cannot open camera" << endl;
+    return;
   }
 
   Mat frame;
@@ -38,7 +66,7 @@ int main() {
       break;
     }
 
-    if(count == 1) {
+    if (count == 1) {
       save(frame);
     }
 
@@ -46,8 +74,6 @@ int main() {
     //   break;
     // }
   }
-
-  return 0;
 }
 
 bool display(Mat frame) {
@@ -68,8 +94,12 @@ bool save(Mat frame) {
   imencode(".jpg", frame, buff, param);
 
   ofstream fout(fn, ios::out | ios::binary);
-  fout.write((char*)&buff[0], buff.size());
-  fout.close();
+  if (fout.is_open()) {
+    fout.write((char*)&buff[0], buff.size());
+    fout.close();
+  } else {
+    cout << "write image file failed." << endl;
+  }
 
   return ret;
 }
